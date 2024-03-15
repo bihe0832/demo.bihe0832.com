72b8e408b0f3fea8581c80e1c38268854eeb570b
子勰 <code@bihe0832.com>
2024-03-15 10:53:13 +0800

    图片压缩

--- tools/book/book.sh
+++ tools/book/book.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # author zixie
-cd ~/zixie/github/demo.bihe0832.com/tools/book/
-for file in ~/zixie/github/demo.bihe0832.com/tools/book/source/*; do
+for file in source/*; do
   filename=$(basename "$file")
   base="${filename%.*}"
   ext="${filename##*.}"
-  if [ ! -e "~/zixie/github/demo.bihe0832.com/tools/book/photos/${base}_optimized.${ext}" ]; then
+  if [ ! -e "photos/${base}_optimized.${ext}" ]; then
+    echo $file
     java -jar  ~/zixie/lib/tinyjpg.jar $file
   fi
 done
-cd ~/zixie/github/demo.bihe0832.com/tools/book/source/
-ls | grep "_photo_optimized.jpg" | xargs -I {} mv {} /Volumes/Document/Documents/github/demo.bihe0832.com/tools/book/photos/
+cd source
+ls | grep "_photo_optimized.jpg" | xargs -I {} mv {} ./../photos/
