af57f605808251c68acdc23be8ef202b40bed489
子勰 <code@bihe0832.com>
2024-04-26 15:46:29 +0800

    优化缩略图

--- tools/book/book.sh
+++ tools/book/book.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 # author zixie
+
 for file in source/*; do
   filename=$(basename "$file")
   base="${filename%.*}"
   ext="${filename##*.}"
-  if [ ! -e "photos/${base}_optimized.${ext}" ]; then
+  if [ ! -e "photos/${filename}" ]; then
     echo $file
-    java -jar  ~/zixie/lib/tinyjpg.jar $file
+    sips -s format jpeg -s formatOptions 80 --resampleHeight 360 $file --out photos/$filename
   fi
 done
-cd source
-ls | grep "_photo_optimized.jpg" | xargs -I {} mv {} ./../photos/
+
+# for photosFile in thumb/*; do
+#   filename=$(basename "$photosFile")
+#   base="${filename%.*}"
+#   ext="${filename##*.}"
+#   if [ ! -e "photos/${base}_optimized.${ext}" ]; then
+#     echo $file
+#     java -jar  ~/zixie/lib/tinyjpg.jar $photosFile
+#   fi
+# done
+
+# cd thumb
+# ls | grep "_photo_optimized.jpg" | xargs -I {} mv {} ./../photos/
